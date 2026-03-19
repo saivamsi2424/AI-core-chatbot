18f66718e3153ec259ab2a4eed56cc7f0efad6ce
Vamshi <saivamshi2424@gmail.com>
2026-03-19 16:08:44 +0530

    Fixed Port for deployment

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,10 @@ mongoose
   .then(() => {
     console.log("MongoDB connected successfully");
 
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
