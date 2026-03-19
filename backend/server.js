1d1cf0b1f597c06762d89cd79134357ef5439845
Vamshi <saivamshi2424@gmail.com>
2026-03-19 17:30:02 +0530

    Optimized server.js fiel

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const chatRoute = require("./routes/chat");
+const authRoute = require("./routes/auth");
+
+const app = express();
+
 app.use(
   cors({
     origin: "*",
@@ -6,35 +15,28 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
-require("dotenv").config();
-const chatRoute = require("./routes/chat");
-const authRoute = require("./routes/auth");
-const mongoose = require("mongoose");
 
-const app = express();
-
-app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/chat", chatRoute);
+
+app.get("/", (req, res) => {
+  res.send("Backend server is running");
+});
+
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected successfully");
 
-    const PORT = process.env.PORT || 5000;
-
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
-    console.error("MongoDB connection failed ");
+    console.error("MongoDB connection failed");
     console.error(err);
   });
-
-app.get("/", (req, res) => {
-  res.send("Backend server is running ");
-});
